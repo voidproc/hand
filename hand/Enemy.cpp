35498c6d8d8633363c72493df004bc0d53923d75
voidProc <voidproc@gmail.com>
2023-10-23 08:10:23 +0900

    update

--- hand/Enemy.cpp
+++ hand/Enemy.cpp
@@ -1,5 +1,6 @@
 ﻿#include "Enemy.h"
 #include "SceneSize.h"
+#include "SpriteSheet.h"
 
 namespace hand
 {
@@ -77,8 +78,9 @@ namespace hand
 
 	void Bird2::draw() const
 	{
-		const int birdFrame = static_cast<int>(Clamp(3 * Periodic::Sawtooth0_1(0.4s, time_.sF()), 0.0, 2.0));
-		TextureAsset(U"Bird")(16 * birdFrame, 0, 16, 20).drawAt(pos_, Palette::White);
+		SpriteSheet::DrawAt(TextureAsset(U"Bird"), 3, pos_, Palette::White, 0.4s, time_.sF());
+		//const int birdFrame = static_cast<int>(Clamp(3 * Periodic::Sawtooth0_1(0.4s, time_.sF()), 0.0, 2.0));
+		//TextureAsset(U"Bird")(16 * birdFrame, 0, 16, 20).drawAt(pos_, Palette::White);
 
 		Enemy::draw();
 	}
---
+++ hand/SpriteSheet.h
@@ -0,0 +1,9 @@
+﻿#pragma once
+
+namespace hand
+{
+	namespace SpriteSheet
+	{
+		RectF DrawAt(const Texture& texture, int frames, const Vec2& pos, const Color& color = Palette::White, const Duration& animSpeed = 0.5s, double t = Scene::Time());
+	}
+}
