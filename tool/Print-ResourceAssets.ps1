f67997351666eb4eb956318d50a8a5d63596fe26
voidProc <voidproc@gmail.com>
2023-11-03 21:09:53 +0900

    Resource

--- tool/Print-ResourceAssets.ps1
+++ tool/Print-ResourceAssets.ps1
@@ -2,6 +2,8 @@ function PrintAssets($dir,$file) {
 	ls "../hand/App/$dir/$file" | ?{ -not $_.PSIsContainer } | %{ "Resource($dir/$([string]$_.Name))" }
 }
 
+PrintAssets "audio" "*.wav"
+PrintAssets "audio" "*.ogg"
 PrintAssets "event" "*.csv"
 PrintAssets "font" "*.ttf"
 PrintAssets "shader" "*.hlsl"
